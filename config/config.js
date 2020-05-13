b7280a0e6ed32eeade6bb902773571883c8426f3
lui54lb3rt0 <luisalbertosantossilva@gmail.com>
2020-05-14 00:07:18 +0100

    Initial commit

---
+++ config/config.js
@@ -0,0 +1,7 @@
+var appConfig={
+    'nome' : '1',
+    'ip' : 'localhost',
+    'sub_msg' : '/brov/state',
+    'pub_msg' : '/brov/body_position_setpoint'
+    
+}
---
+++ js/clone-form-td.js
@@ -0,0 +1,51 @@
+/*
+Author: Tristan Denyer (based on Charlie Griefer's original clone code, and some great help from Dan - see his comments in blog post)
+Plugin and demo at http://tristandenyer.com/using-jquery-to-duplicate-a-section-of-a-form-maintaining-accessibility/
+Ver: 0.9.3
+Date: Mar 3, 2013
+*/
+$(function () {
+    $('#btnAdd').click(function () {
+        var num     = $('.clonedInput').length, // how many "duplicatable" input fields we currently have
+            newNum  = new Number(num + 1),      // the numeric ID of the new input field being added
+            newElem = $('#entry' + num).clone().attr('id', 'entry' + newNum).fadeIn('slow'); // create the new element via clone(), and manipulate it's ID using newNum value
+    // manipulate the name/id values of the input inside the new element
+        // H2 - section
+        newElem.find('.heading-reference').attr('id', 'ID' + newNum + '_reference').attr('name', 'ID' + newNum + '_reference').html('Entry #' + newNum);
+
+
+    // insert the new element after the last "duplicatable" input field
+        $('#entry' + num).after(newElem);
+        $('#ID' + newNum + '_title').focus();
+
+    // enable the "remove" button
+        $('#btnDel').attr('disabled', false);
+
+    // right now you can only add 5 sections. change '5' below to the max number of times the form can be duplicated
+        if (newNum == 5)
+        $('#btnAdd').attr('disabled', true).prop('value', "You've reached the limit");
+    });
+
+    $('#btnDel').click(function () {
+    // confirmation
+        if (confirm("Are you sure you wish to remove this section? This cannot be undone."))
+            {
+                var num = $('.clonedInput').length;
+                // how many "duplicatable" input fields we currently have
+                $('#entry' + num).slideUp('slow', function () {$(this).remove(); 
+                // if only one element remains, disable the "remove" button
+                    if (num -1 === 1)
+                $('#btnDel').attr('disabled', true);
+                // enable the "add" button
+                $('#btnAdd').attr('disabled', false).prop('value', "add section");});
+            }
+        return false;
+             // remove the last element
+
+    // enable the "add" button
+        $('#btnAdd').attr('disabled', false);
+    });
+
+    $('#btnDel').attr('disabled', true);
+
+});
